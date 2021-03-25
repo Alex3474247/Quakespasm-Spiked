e560390d86cc143a682566e67e3ff75fe29b1a12
Emile Belanger <emile.belanger@gmail.com>
2021-03-25 18:09:37 +0000

    Updated for refactored touch interface

--- quakespasm/mobile/game_interface.c
+++ quakespasm/mobile/game_interface.c
@@ -219,12 +219,6 @@ touchscreemode_t PortableGetScreenMode()
 		return TS_MENU;
 }
 
-
-void PortableAutomapControl(float zoom, float x, float y)
-{
-
-}
-
 void PortableBackButton()
 {
     LOGI("Back button");
